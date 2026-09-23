3bbda6e693cd4a7b4a058026aeca7084746be56d
SUDHANSHU DUBEY <manudubey249@gmail.com>
2026-09-23 22:29:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 
 app.use(cors({
-  origin: ["https://filmflixnow-liard.vercel.app"], 
+  origin: ["https://filmflixnow-liard.vercel.app""https://movie-site-chi-nine.vercel.app"], 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
